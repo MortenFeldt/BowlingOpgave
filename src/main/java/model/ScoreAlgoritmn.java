24bf9a7721481fa6429646d45ac427024ed41d32
MortenFeldtStudent <cph-mf227@cphbusiness.dk>
2020-07-27 19:59:34 +0200

    Code clean up
    Better code overview.

--- src/main/java/model/ScoreAlgoritmn.java
+++ src/main/java/model/ScoreAlgoritmn.java
@@ -11,11 +11,11 @@ package model;
 public class ScoreAlgoritmn {
 
     public int[] scoreAlgoritmn(int[][] arr) {
-        int[] samletScore = new int[arr.length];
         final int MAXSCORE = 10;
         int currentScore = 0;
         boolean spare = false;
         boolean strike = false;
+        int[] samletScore = new int[arr.length];
 
         for (int i = 0; i < arr.length; i++) {
             int[] currentArr = arr[i];
@@ -60,39 +60,4 @@ public class ScoreAlgoritmn {
 
         return samletScore;
     }
-    
-//    public int[] scoreAlgoritmn(int[][] arr) {
-//        int[] samletScore = new int[arr.length];
-//        final int MAXSCORE = 10;
-//        int currentScore = 0;
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            int[] currentArr = arr[i];
-//            currentScore += currentArr[0];
-//            currentScore += currentArr[1];
-//            if (i > 0) {
-//                currentScore += samletScore[i - 1];
-//            }
-//
-//            if (currentArr[0] == MAXSCORE) {
-//                if(i < arr.length - 1){
-//                    if (arr[i + 1][0] == MAXSCORE) {
-//                        currentScore += arr[i + 1][0];
-//                        if(i + 2 <= arr.length - 1){
-//                            currentScore += arr[i + 2][0];
-//                        }
-//                    } else {
-//                        currentScore += arr[i + 1][0];
-//                        currentScore += arr[i + 1][1];
-//                    }
-//                }
-//
-//            }
-//
-//            samletScore[i] = currentScore;
-//            currentScore = 0;
-//        }
-//
-//        return samletScore;
-//    }
 }
